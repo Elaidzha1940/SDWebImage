5e33a6c90d8c33b211824c510ea53ceb91d34979
Elaidzha1940 <eli080293@gmail.com>
2024-03-28 16:22:55 +0300

    SDWebImage

---
+++ SDWebImage/ContentView.swift
@@ -0,0 +1,22 @@
+//  /*
+//
+//  Project: SDWebImage
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 28.03.2024
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SDWebImage/SDWebImageApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: SDWebImage
+//  File: SDWebImageApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 28.03.2024
+//
+//  */
+
+import SwiftUI
+
+@main
+struct SDWebImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
