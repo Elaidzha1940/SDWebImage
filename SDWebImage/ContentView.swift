94205873c5f43081954f4f9a39e088d92647fd4d
Elaidzha1940 <eli080293@gmail.com>
2024-03-28 16:31:48 +0300

    Update

--- SDWebImage/ContentView.swift
+++ SDWebImage/ContentView.swift
@@ -8,6 +8,7 @@
 //  */
 
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct ContentView: View {
     var body: some View {
@@ -20,3 +21,19 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
+
+//WebImage(url: URL(string: "https://nokiatech.github.io/heif/content/images/ski_jump_1440x960.heic")) { image in
+//    image.resizable() // Control layout like SwiftUI.AsyncImage, you must use this modifier or the view will use the image bitmap size
+//} placeholder: {
+//    Rectangle().foregroundColor(.gray)
+//}
+//// Supports options and context, like `.delayPlaceholder` to show placeholder only when error
+//.onSuccess { image, data, cacheType in
+//    // Success
+//    // Note: Data exist only when queried from disk cache or network. Use `.queryMemoryData` if you really need data
+//}
+//.indicator(.activity) // Activity Indicator
+//.transition(.fade(duration: 0.5)) // Fade Transition with duration
+//.scaledToFit()
+//.frame(width: 300, height: 300, alignment: .center)
