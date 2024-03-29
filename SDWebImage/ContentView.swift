61a275e20da0e98339f40d57baed027acef1fa51
Elaidzha1940 <eli080293@gmail.com>
2024-03-29 03:59:30 +0300

    Update ContentView

--- SDWebImage/ContentView.swift
+++ SDWebImage/ContentView.swift
@@ -13,7 +13,25 @@ import SDWebImageSwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            
+            WebImage(url: URL(string: "https://picsum.photos/200/300?grayscaleh")) { image in
+                
+                image.resizable()
+                
+            } placeholder: {
+                Rectangle()
+                    .foregroundColor(.gray)
+                //                    .overlay(
+                //                        ProgressView()
+                //                    )
+            }
+            //            .onSuccess(perform: { image, data, cacheType in
+            //                //
+            //            }
+            //                .onFailure(perform: { error in
+            //                    //
+            //                }
+            .indicator(.activity)
+            .aspectRatio(contentMode: .fit)
         }
     }
 }
@@ -22,18 +40,3 @@ struct ContentView: View {
     ContentView()
 }
 
-
-//WebImage(url: URL(string: "https://nokiatech.github.io/heif/content/images/ski_jump_1440x960.heic")) { image in
-//    image.resizable() // Control layout like SwiftUI.AsyncImage, you must use this modifier or the view will use the image bitmap size
-//} placeholder: {
-//    Rectangle().foregroundColor(.gray)
-//}
-//// Supports options and context, like `.delayPlaceholder` to show placeholder only when error
-//.onSuccess { image, data, cacheType in
-//    // Success
-//    // Note: Data exist only when queried from disk cache or network. Use `.queryMemoryData` if you really need data
-//}
-//.indicator(.activity) // Activity Indicator
-//.transition(.fade(duration: 0.5)) // Fade Transition with duration
-//.scaledToFit()
-//.frame(width: 300, height: 300, alignment: .center)
